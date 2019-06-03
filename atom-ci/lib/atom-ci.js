5d1a5576d7604fe351e8fb07bbfc7f1913750a5d
Richie Bendall <richiebendall@gmail.com>
2019-06-03 20:20:46 +1200

    Update code

---
+++ atom-ci/lib/atom-ci.js
@@ -0,0 +1,54 @@
+'use babel';
+
+import AtomCiView from './atom-ci-view';
+import {
+    CompositeDisposable
+} from 'atom';
+
+export default {
+
+    atomCiView: null,
+    modalPanel: null,
+    subscriptions: null,
+
+    activate(state) {
+        this.atomCiView = new AtomCiView(state.atomCiViewState);
+        this.modalPanel = {
+            element: document.createElement('div'),
+            getTitle: () => 'Continuous Integration',
+            getURI: () => 'atom://atom-ci/atom-ci-view',
+            getDefaultLocation: () => 'bottom'
+        };
+        atom.workspace.open(this.modalPanel);
+
+        // Events subscribed to in atom's system can be easily cleaned up with a CompositeDisposable
+        this.subscriptions = new CompositeDisposable();
+
+        // Register command that toggles this view
+        this.subscriptions.add(atom.commands.add('atom-workspace', {
+            'atom-ci:toggle': () => atom.workspace.toggle(this.modalPanel)
+        }));
+    },
+
+    deactivate() {
+        this.modalPanel.destroy();
+        this.subscriptions.dispose();
+        this.atomCiView.destroy();
+    },
+
+    serialize() {
+        return {
+            atomCiViewState: this.atomCiView.serialize()
+        };
+    },
+
+    toggle() {
+        console.log('AtomCi was toggled!');
+        return (
+            this.modalPanel.isVisible() ?
+            this.modalPanel.hide() :
+            this.modalPanel.show()
+        );
+    }
+
+};
---
+++ atom-ci/spec/atom-ci-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomCiView from '../lib/atom-ci-view';
+
+describe('AtomCiView', () => {
+    it('has one valid test', () => {
+        expect('life').toBe('easy');
+    });
+});
